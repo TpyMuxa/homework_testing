e1a21ecd6008ba17ab7e8ec589c04a8c5f5dd323
TpyMuxa <tpymuxa1133@gmail.com>
2024-01-13 14:20:54 +0500

    Initial commit

---
+++ src/main/java/org/example/controller/CalculatorController.java
@@ -0,0 +1,62 @@
+package org.example.controller;
+
+import org.example.service.CalculatorService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Добро пожаловать в калькулятор !";
+    }
+
+    @GetMapping("/plus")
+    public String plus(@RequestParam(value = "num1", required = false) Integer a,
+                       @RequestParam(value = "num2", required = false) Integer b) {
+        return calculate(a, b, "+");
+    }
+
+    @GetMapping("/minus")
+    public String minus(@RequestParam(value = "num1", required = false) Integer a,
+                        @RequestParam(value = "num2", required = false) Integer b) {
+        return calculate(a, b, "-");
+    }
+
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam(value = "num1", required = false) Integer a,
+                           @RequestParam(value = "num2", required = false) Integer b) {
+        return calculate(a, b, "*");
+    }
+
+    @GetMapping("/divide")
+    public String divide(@RequestParam(value = "num1", required = false) Integer a,
+                         @RequestParam(value = "num2", required = false) Integer b) {
+        return calculate(a, b, "/");
+    }
+
+    private String calculate(Integer a, Integer b, String operation) {
+        if (a == null || b == null) {
+            return "Какой-то из параметров не передан!";
+        }
+        if (operation.equals("/") && b == 0) {
+            return "На ноль делить нельзя !";
+        }
+        int result = switch (operation) {
+            case "-" -> calculatorService.minus(a, b);
+            case "*" -> calculatorService.multiply(a, b);
+            case "/" -> calculatorService.divide(a, b);
+            default -> calculatorService.plus(a, b);
+        };
+        return "%d %s %d = %d".formatted(a, operation, b, result);
+    }
+}
